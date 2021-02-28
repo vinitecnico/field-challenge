d5dc8840529fe487e325c21e3de4339fca5a4c52
vinicus rodrigues <vinitecnico@gmail.com>
2021-02-28 20:30:44 -0300

    ajusted layout master page

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./styles.scss";
 
 const Content = ({ noBgColor, children, ...props}) => {
     return (
-        <main className={`main`}>
-            <div className={`content col-10 offset-1 ${noBgColor && 'no-bg-color'}`}>
+        <main className="content">
+            <div className={`col-10 offset-1 ${noBgColor && 'no-bg-color'}`}>
                 {children}
             </div>
         </main>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import history from "../../history";
 import "./styles.scss";
 
 const Header = () => {
   return (
     <header>
-      <nav className="col-md-10">
-        <section>
-          <img
-            onClick={() => history.push("/")}
-            src={logo}
-            title="CityBikes"
-            alt="CityBikes"
-          />
-          <h1>City Bikes - Serving the DC Cycling Community since 1988</h1>
-        </section>
-      </nav>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="site-logo mb-4">
+              <img src={logo} alt="city bikes" />
+            </div>
+            <h1 className="text-white text-center text-shadow">
+              City Bikes - Serving the DC Cycling Community since 1988
+            </h1>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
